d8d96cb8c5019e86a6badd9a5b503319be8f4ad6
Shubham Singh <shubham110084@outlook.com>
2026-03-20 17:49:16 +0530

    add empty progress page with header

---
+++ app/progress/page.js
@@ -0,0 +1,15 @@
+export default async function Progress() {
+    return (
+        <div className="min-h-screen border-l border-gray-200 w-full">
+            <header className="border-b border-gray-200 py-4 px-8">
+                <h1 className="text-3xl font-bold">
+                    Productivity Tracking
+                </h1>
+            </header>
+
+            <section>
+                {/* Create charts here */}
+            </section>
+        </div>
+    );
+}
