493d65a6dcf78dbd39e6a77197cb39ac50f65cf5
Shubham Singh <shubham110084@outlook.com>
2026-03-21 17:20:33 +0530

    configure redux and create authSlice and store

---
+++ app/redux/slices/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name: "authSlice",
+    initialState: {
+        isAuthenticated: false
+    },
+    reducers: {
+        setIsAuthenticated: (state = initialState, action) => {
+            state.isAuthenticated = action.payload
+        }
+    }
+});
+
+export const { setIsAuthenticated } = authSlice.actions;
