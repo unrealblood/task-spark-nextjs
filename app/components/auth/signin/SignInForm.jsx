1f5d09dd1e227a60bfd36bb558010d5b91314f62
Shubham Singh <shubham110084@outlook.com>
2026-03-21 10:59:23 +0530

    create dummy signin page and some minor tweaks

--- app/components/auth/signin/SignInForm.jsx
+++ app/components/auth/signin/SignInForm.jsx
@@ -2,20 +2,13 @@
 
 import { useState } from "react";
 
-export default function SignupForm() {
-    const [name, setName] = useState("");
+export default function SigninForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
 
     return (
         <div>
             <form className="mt-8 flex justify-center items-center gap-4 flex-col">
-                <div className="flex justify-start items-start flex-col gap-1 w-full">
-                    <label htmlFor="inputName">Full Name</label>
-                    <input type="text" id="inputName" name="name" placeholder="Enter your full name" className="bg-gray-200 p-2 rounded-lg w-full" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-
                 <div className="flex justify-start items-start flex-col gap-1 w-full">
                     <label htmlFor="inputEmail">Email Address</label>
                     <input type="email" id="inputEmail" name="email" placeholder="Enter your email address" className="bg-gray-200 p-2 rounded-lg w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
@@ -27,12 +20,7 @@ export default function SignupForm() {
                 </div>
 
                 <div className="flex justify-start items-start flex-col gap-1 w-full">
-                    <label htmlFor="inputConfirmPassword">Confirm Password</label>
-                    <input type="password" id="inputConfirmPassword" name="confirmPassword" placeholder="Enter your email address" className="bg-gray-200 p-2 rounded-lg w-full" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                </div>
-
-                <div className="flex justify-start items-start flex-col gap-1 w-full">
-                    <button type="submit" className="bg-blue-500 rounded-lg text-white py-2 w-full cursor-pointer">Create Account</button>
+                    <button type="submit" className="bg-blue-500 rounded-lg text-white py-2 w-full cursor-pointer">Sign In</button>
                 </div>
             </form>
         </div>
