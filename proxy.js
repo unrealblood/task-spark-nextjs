8c7098f74d3d44e678412177a6afd9f69031fd27
Shubham Singh <shubham110084@outlook.com>
2026-03-22 20:58:18 +0530

    add more routes to protect from accessing by unauth users

--- proxy.js
+++ proxy.js
@@ -4,11 +4,8 @@ export function proxy(request) {
     // Check if the secure session cookie exists
     const session = request.cookies.get('session')?.value;
 
-    console.log("Session Cookie Value");
-    console.log(request.cookies.get('session')?.value);
-
     // If there is no session cookie and the user is trying to access the home route
-    if (!session && request.nextUrl.pathname === '/') {
+    if (!session && (request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/add-task' || request.nextUrl.pathname === '/progress' || request.nextUrl.pathname === '/settings')) {
     // Redirect them to the login page
         return NextResponse.redirect(new URL('/auth/signin', request.url));
     }
@@ -19,5 +16,5 @@ export function proxy(request) {
 
 // Specify which routes this middleware should run on
 export const config = {
-    matcher: ['/'], // Add other protected routes here like '/dashboard/:path*'
+    matcher: ['/', "/add-task", "/progress", "/settings"], // Add other protected routes here like '/dashboard/:path*'
 };
