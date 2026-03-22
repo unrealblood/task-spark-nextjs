873fa21a9144665344c80901c8d1e4ab3a7da895
Shubham Singh <shubham110084@outlook.com>
2026-03-22 15:03:55 +0530

    add sign-in and sign-up functionality with firebase-auth without validation

--- app/components/auth/signin/SignInForm.jsx
+++ app/components/auth/signin/SignInForm.jsx
@@ -1,14 +1,34 @@
 "use client";
 
+import { app } from "@/lib/firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 
 export default function SigninForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const firebaseAuth = getAuth(app);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        signInWithEmailAndPassword(firebaseAuth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+
+            if(user) {
+                console.log("Signed in successfully.");
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    }
+
     return (
         <div>
-            <form className="mt-8 flex justify-center items-center gap-4 flex-col">
+            <form onSubmit={handleSubmit} className="mt-8 flex justify-center items-center gap-4 flex-col">
                 <div className="flex justify-start items-start flex-col gap-1 w-full">
                     <label htmlFor="inputEmail">Email Address</label>
                     <input type="email" id="inputEmail" name="email" placeholder="Enter your email address" className="bg-gray-200 p-2 rounded-lg w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
--- app/components/auth/signup/SignupForm.jsx
+++ app/components/auth/signup/SignupForm.jsx
@@ -1,20 +1,42 @@
 "use client";
 
+import { app } from "@/lib/firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
 
 export default function SignupForm() {
-    const [name, setName] = useState("");
+    //const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const firebaseAuth = getAuth(app);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        createUserWithEmailAndPassword(firebaseAuth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            
+            if(user) {
+                console.log("User successfully signed up. You can now login.");
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    }
+
     return (
         <div>
-            <form className="mt-8 flex justify-center items-center gap-4 flex-col">
+            <form onSubmit={handleSubmit} className="mt-8 flex justify-center items-center gap-4 flex-col">
+                {/*
                 <div className="flex justify-start items-start flex-col gap-1 w-full">
                     <label htmlFor="inputName">Full Name</label>
                     <input type="text" id="inputName" name="name" placeholder="Enter your full name" className="bg-gray-200 p-2 rounded-lg w-full" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
+                */}
 
                 <div className="flex justify-start items-start flex-col gap-1 w-full">
                     <label htmlFor="inputEmail">Email Address</label>
