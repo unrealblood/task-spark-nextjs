66ee3235e820555f0f0bd58eb902a37e144cb625
Shubham Singh <shubham110084@outlook.com>
2026-03-24 13:12:34 +0530

    add logo to header component and allowed images from cloudinary host in next.config.mjs file

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import HeaderProvider from "./HeaderProvider";
+import Image from "next/image";
 
 export default async function Header() {
     return (
         <header className="flex flex-col h-screen">
-            <h1 className="pr-24 pl-4 py-4 text-3xl font-bold shrink-0">
-                <Link href="/">Task Spark</Link>
+            <h1 className="pl-4 py-4 text-3xl font-bold shrink-0">
+                <Link href="/" className="flex justify-start items-center">
+                    <div className="w-auto h-auto">
+                        <Image src="https://res.cloudinary.com/diehhord1/image/upload/v1774337831/task_spark_MAUI_app_icon_lsk0kt.png" width={50} height={50} priority alt="task-spark-logo" />
+                    </div>
+                    <h1>Task Spark</h1>
+                </Link>
             </h1>
             
             <HeaderProvider />
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          pathname: "/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
